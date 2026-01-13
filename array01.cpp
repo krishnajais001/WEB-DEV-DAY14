04034c43d2bd9c629f31a7200237e9b7dfd7b67c
Krishna Kumar <krishnajais7091@gmail.com>
2026-01-14 03:58:50 +0530

    bbfbf

--- array01.cpp
+++ array01.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
 using namespace std;
 
-void printArray(int arr[], int size){
+// void printArray(int arr[], int size){
+//     for(int i = 0; i<size;i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     }
+
+// Count 0 or 1
+void printArrayZeroOrOne(int arr[], int size){
+    int zero=0;
+    int one=1;
     for(int i = 0; i<size;i++){
-        cout<<arr[i]<<" ";
+        if(arr[i]==0){
+            zero++;
+        }else if(arr[i]==1){
+            one++;
+        }
     }
+    cout<<"Total Zero :"<<zero<<endl;
+     cout<<"Total One :"<<one<<endl;
     }
+
+    
+    
+    // function to print twice of the original value
+    void printEvenOrOdd(int arr[], int size){
+    for(int i = 0; i<size;i++){
+        if(arr[i]%2 == 0){
+            cout<<"Even :"<<arr[i]<<" ";
+        }
+        if (arr[i]%2!=0){
+             cout<<"Odd :"<<arr[i]<<" ";
+        }
+        
+    }
+    
+}
+
+
+    
 
 int main(){
 
@@ -50,21 +84,30 @@ int main(){
 
 
 
-    char arr[10];
-    int i=0;
+    // char arr[10];
+    // int i=0;
    
-    for(int i=0; i<10;i++){
-         cout<<"Enter input for indexes :";
-        cin>>arr[i];
+    // for(int i=0; i<10;i++){
+    //      cout<<"Enter input for indexes :";
+    //     cin>>arr[i];
         
-    }
-    cout<<endl;
-    cout<<"Char Array : ";
-    for(int i=0; i<10;i++){
-        cout<<arr[i]<<" ";
-    }
+    // }
+    // cout<<endl;
+    // cout<<"Char Array : ";
+    // for(int i=0; i<10;i++){
+    //     cout<<arr[i]<<" ";
+    // }
     
+int arr[10] = {1,1,0,1,0};
+int size =5;
+// printArray(arr,size);
 
+// cout<<endl;
+// printTwice(arr,size);
+
+// printEvenOrOdd(arr,size);
+
+printArrayZeroOrOne(arr,size);
 
     return 0;
 }
